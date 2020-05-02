ad002013c17ad31cbdb44c0af749a75ca0010dbf
Sai Sukesh <sukesh.sai04@gmail.com>
2020-05-02 20:34:08 +0530

    Initial commit

---
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -0,0 +1,30 @@
+package com.udacity.gradle.builditbigger.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import javax.inject.Named;
+
+/** An endpoint class we are exposing */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.builditbigger.gradle.udacity.com",
+                ownerName = "backend.builditbigger.gradle.udacity.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /** A simple endpoint method that takes a name and says Hi back */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+
+}
